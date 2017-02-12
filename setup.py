50d1a0d5b23c29cd73e62022e0d8ac61ad2329c7
Richard Cook <rcook@rcook.org>
2017-02-12 09:54:23 -0800

    Create README.rst and manifest

--- setup.py
+++ setup.py
@@ -10,13 +10,19 @@
 from __future__ import print_function
 from setuptools import setup
 
+def _read_readme():
+    with open("README.md") as f:
+        return f.read()
+
 setup(
     name="pyprelude",
     version="0.1",
     description="General-purpose Python support functions",
+    long_description=_read_readme(),
     url="https://github.com/rcook/pyprelude",
     author="Richard Cook",
     author_email="rcook@rcook.org",
     license="MIT",
     packages=["pyprelude"],
+    include_package_data=True,
     zip_safe=False)
