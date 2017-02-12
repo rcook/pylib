306181c4b44a135da5bf4df367a5a90176e73c22
Richard Cook <rcook@rcook.org>
2017-02-12 09:20:25 -0800

    Create setup.py script

---
+++ setup.py
@@ -0,0 +1,22 @@
+############################################################
+#
+# pyprelude
+# Copyright (C) 2017, Richard Cook
+# Release under MIT License
+# https://github.com/rcook/pyprelude
+#
+############################################################
+
+from __future__ import print_function
+from setuptools import setup
+
+setup(
+    name="pyprelude",
+    version="0.1",
+    description="General-purpose Python support functions",
+    url="https://github.com/rcook/pyprelude",
+    author="Richard Cook",
+    author_email="rcook@rcook.org",
+    license="MIT",
+    packages=["pyprelude"],
+    zip_safe=False)
