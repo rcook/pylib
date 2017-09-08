63cd1356d652e24813dd0409c6fe5a0703d8965d
Richard Cook <rcook@rcook.org>
2017-09-08 10:06:10 -0700

    Fixes #2: Fall back to robocopy on Windows in remove_dir

--- pyprelude/file_system.py
+++ pyprelude/file_system.py
@@ -7,23 +7,29 @@
 #
 ############################################################
 
-from __future__ import print_function
 import os
 import shutil
 import stat
 
 from .util import unpack_args
 
-def _on_error(func, path, exc_info):
+def _remove_dir_on_error(func, path, exc_info):
     if os.access(path, os.W_OK):
         raise
     else:
         os.chmod(path, stat.S_IWUSR)
         func(path)
 
-def remove_dir(dir):
-    # TODO: Consider using robocopy on Windows
-    shutil.rmtree(dir, onerror=_on_error)
+def remove_dir(path):
+    try:
+        shutil.rmtree(path, onerror=_remove_dir_on_error)
+    except WindowsError:
+        with temp_dir() as d:
+            status, output, error = execute("robocopy.exe", d, path, "/mir", can_fail=True)
+            if status != 2:
+                raise RuntimeError("robocopy failed with status {} (output={}, error={})".format(status, output, error))
+
+        os.rmdir(path)
 
 def make_path(*args):
     """
